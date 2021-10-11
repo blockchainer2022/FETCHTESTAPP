da8fabbbb3953bc66fc9b50eef58ea04e974f4f3
blockchainer2022 <=blockchainer.2202@gmail.com>
2021-10-11 17:06:52 +0530

    redirect solve

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -2,24 +2,40 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 const Test = ({ match }) => {
   const [data, SetData] = useState({});
+  const [loading, SetLoading] = useState(true);
+  const [error, SetError] = useState(false);
 
   const { id } = match.params;
 
   useEffect(() => {
     const getData = async () => {
-      const { data } = await axios.get(
-        `https://ipfs.io/ipfs/QmbKjG9DR2KfHmdkjbuRDnXWkm84BUnrYBm3aWDc4pikAT/${id}`
-      );
-      console.log(data);
-      SetData(data);
+      try {
+        SetLoading(true);
+        const { data } = await axios.get(
+          `https://ipfs.io/ipfs/QmbKjG9DR2KfHmdkjbuRDnXWkm84BUnrYBm3aWDc4pikAT/${id}`
+        );
+
+        SetData(data);
+        SetLoading(false);
+      } catch (error) {
+        SetError(true);
+      }
     };
     getData();
   }, [id]);
 
   return (
     <div>
-      <h1>{data.name}</h1>
-      <img src={data.image} alt="dataImage" style={{ width: 300 }} />
+      {error ? (
+        "Sorry Something is Wrong "
+      ) : loading ? (
+        "Wait Data is on the way"
+      ) : (
+        <>
+          <h1>{data.name}</h1>
+          <img src={data.image} alt="dataImage" style={{ width: 300 }} />
+        </>
+      )}
     </div>
   );
 };

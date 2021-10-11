d850a1d20364cd9571bdc0e5006a49b1a77c604c
blockchainer2022 <=blockchainer.2202@gmail.com>
2021-10-11 16:59:11 +0530

    redirect solve

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -2,7 +2,9 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 const Test = ({ match }) => {
   const [data, SetData] = useState({});
+
   const { id } = match.params;
+
   useEffect(() => {
     const getData = async () => {
       const { data } = await axios.get(

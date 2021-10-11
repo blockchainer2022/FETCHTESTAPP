f3815ee0daae82fa45b4f4b6f45264d30892d59c
blockchainer2022 <=blockchainer.2202@gmail.com>
2021-10-11 16:51:46 +0530

    initial

---
+++ src/pages/Test.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+const Test = ({ match }) => {
+  const [data, SetData] = useState({});
+  const { id } = match.params;
+  useEffect(() => {
+    const getData = async () => {
+      const { data } = await axios.get(
+        `https://ipfs.io/ipfs/QmbKjG9DR2KfHmdkjbuRDnXWkm84BUnrYBm3aWDc4pikAT/${id}`
+      );
+      console.log(data);
+      SetData(data);
+    };
+    getData();
+  }, [id]);
+
+  return (
+    <div>
+      <h1>{data.name}</h1>
+      <img src={data.image} alt="dataImage" style={{ width: 300 }} />
+    </div>
+  );
+};
+
+export default Test;
